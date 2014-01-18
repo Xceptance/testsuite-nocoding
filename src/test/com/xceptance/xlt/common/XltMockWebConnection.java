fd4b508659338e3f9c31508e786f9ce609d3e0e2
Rene Schwietzke <r.schwietzke@xceptance.net>
2014-01-18 13:42:24 +0100

    Added Apache license header

--- src/test/com/xceptance/xlt/common/XltMockWebConnection.java
+++ src/test/com/xceptance/xlt/common/XltMockWebConnection.java
@@ -1,9 +1,18 @@
-/*
- * File: XltMockWebConnection.java
- * Created on: Nov 11, 2009
- * 
- * Copyright 2009
- * Xceptance Software Technologies GmbH, Germany.
+/**  
+ *  Copyright 2014 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ *
  */
 package test.com.xceptance.xlt.common;
 
@@ -21,7 +30,6 @@ import com.xceptance.xlt.engine.XltWebClient;
  * </p>
  * 
  * @author Hartmut Arlt (Xceptance Software Technologies GmbH)
- * @version $Id: XltMockWebConnection.java 4715 2010-08-24 13:32:35Z jwerner $
  */
 public class XltMockWebConnection extends MockWebConnection
 {
